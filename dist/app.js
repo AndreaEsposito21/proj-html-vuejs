7b3f25bfa1a0df909c6e1b8e29b28246036b8a16
Andrea Esposito <andexpox@gmail.com>
2021-05-10 20:51:59 +0200

    navbar-logo-bg

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,23 @@
 
 var app = new Vue({
   el: '#root',
-  data: {},
+  data: {
+    navbar: [{
+      navitem: 'Home'
+    }, {
+      navitem: 'About Me'
+    }, {
+      navitem: 'Testimonials'
+    }, {
+      navitem: 'My Blog'
+    }, {
+      navitem: 'Meetups'
+    }, {
+      navitem: 'Shop'
+    }, {
+      navitem: 'Contact Me'
+    }]
+  },
   methods: {},
   mounted: function mounted() {}
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 var app = new Vue ({
     el: '#root',
     data: {
-        
+        navbar: [
+            { navitem: 'Home' },
+            { navitem: 'About Me' },
+            { navitem: 'Testimonials' },
+            { navitem: 'My Blog' },
+            { navitem: 'Meetups' },
+            { navitem: 'Shop' },
+            { navitem: 'Contact Me' }
+        ]
     },
     methods: {
 
