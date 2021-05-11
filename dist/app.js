4e2f487d79fae9f10e1df698cdd1b9b4b9553970
Andrea Esposito <andexpox@gmail.com>
2021-05-11 20:21:31 +0200

    section-1-2-main

--- dist/app.js
+++ dist/app.js
@@ -24,6 +24,24 @@ var app = new Vue({
       navitem: 'Shop'
     }, {
       navitem: 'Contact Me'
+    }, {
+      navitemImg: 'shopping-cart.svg'
+    }],
+    appLogo: [{
+      imgFile: '/ebay-logotan-2x.png'
+    }, {
+      imgFile: '/audible-logotan-2x.png'
+    }, {
+      imgFile: '/bb-logotan-2x.png'
+    }, {
+      imgFile: '/kindlefire-logotan-2x.png'
+    }],
+    logoBook: [{
+      logoFile: '/audible-logo.png'
+    }, {
+      logoFile: '/bn-logo-tall.png'
+    }, {
+      logoFile: '/kindle-logo-tall.png'
     }]
   },
   methods: {},
--- src/app.js
+++ src/app.js
@@ -8,7 +8,21 @@ var app = new Vue ({
             { navitem: 'My Blog' },
             { navitem: 'Meetups' },
             { navitem: 'Shop' },
-            { navitem: 'Contact Me' }
+            { navitem: 'Contact Me' },
+            { navitemImg: 'shopping-cart.svg' }
+        ],
+
+        appLogo: [
+            { imgFile: '/ebay-logotan-2x.png'},
+            { imgFile: '/audible-logotan-2x.png'},
+            { imgFile: '/bb-logotan-2x.png'},
+            { imgFile: '/kindlefire-logotan-2x.png'}
+        ],
+
+        logoBook: [
+            { logoFile: '/audible-logo.png' },
+            { logoFile: '/bn-logo-tall.png' },
+            { logoFile: '/kindle-logo-tall.png' }
         ]
     },
     methods: {
